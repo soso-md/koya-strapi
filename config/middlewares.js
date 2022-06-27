0f8497d8c5be63a33803c435d74e5ce7ac4be269
Sophie Raps <s.raps@onveda>
2022-06-27 17:46:06 +0200

    added cloudanry

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,6 @@
 module.exports = [
   "strapi::errors",
-  "strapi::security",
+  // "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
@@ -9,4 +9,30 @@ module.exports = [
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "res.cloudinary.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,3 @@
-// module.exports = () => {
-//   return {
-//     ckeditor: true,
-//     upload: {
-//       breakpoints: {
-//         xlarge: 1920,
-//         large: 1000,
-//         medium: 750,
-//         small: 500,
-//         xsmall: 64,
-//       },
-//     },
-//   };
-// };
-
 module.exports = ({ env }) => ({
   upload: {
     config: {
@@ -22,6 +7,17 @@ module.exports = ({ env }) => ({
         medium: 750,
         small: 500,
         xsmall: 64,
+      },
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
