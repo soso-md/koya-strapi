8f9de043a639f27700f0b1805a6d7cc97a2ca8b3
Sophie Raps <s.raps@onveda>
2022-06-27 19:01:23 +0200

    changed content types

--- src/api/blogpost/controllers/blogpost.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  blogpost controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::blogpost.blogpost');
--- src/api/blogpost/services/blogpost.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * blogpost service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::blogpost.blogpost');
