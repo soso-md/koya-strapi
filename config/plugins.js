ff348fe36c1f9b26ca1e29b80c85b73c1fa277d0
Sophie Raps <s.raps@onveda>
2022-06-27 19:12:40 +0200

    added graphql config

--- config/plugins.js
+++ config/plugins.js
@@ -21,4 +21,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
 });
